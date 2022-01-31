17b87ff4f0dbf5127a718e141e876c0958a50778
Michael Smith <m.t.smith@sheffield.ac.uk>
2022-01-31 23:27:11 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'ecolab3',
+  packages = ['ecolab3'],
+  version = '3.00',
+  description = 'Agent Based Model for COM3001/6009',
+  author = 'Mike Smith',
+  author_email = 'm.t.smith@sheffield.ac.uk',
+  url = 'https://github.com/lionfish0/ecolab3.git',
+  download_url = 'https://github.com/lionfish0/ecolab3.git',
+  keywords = ['agent based model','teaching','rabbit','fox'],
+  classifiers = [],
+  install_requires=['numpy'],
+)
+
